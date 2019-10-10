86ba6b8598e93d39a451a5d4b2f951954456de9d
Rafael Cintra Lyra <ruflescl@gmail.com>
2019-10-09 21:13:55 -0300

    Initial commit

---
+++ app/src/main/java/com/pinkyra/kotlinrepoexplorer/feature/explorer/ExplorerActivity.kt
@@ -0,0 +1,13 @@
+package com.pinkyra.kotlinrepoexplorer.feature.explorer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pinkyra.kotlinrepoexplorer.R
+
+class ExplorerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_explorer)
+    }
+}
